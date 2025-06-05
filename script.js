6056e6703de259c837bc0f20af7c7f860ba0ad80
Matteo <mt-sette@proton.me>
2025-06-05 11:30:46 +0200

    Render markdown  editor

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-import { EditorView, basicSetup } from 'https://esm.sh/@codemirror/basic-setup';
+import { EditorView } from 'https://esm.sh/@codemirror/view';
 import { EditorState } from 'https://esm.sh/@codemirror/state';
+import { basicSetup } from 'https://esm.sh/codemirror';
 import { markdown } from 'https://esm.sh/@codemirror/lang-markdown';
 
 // Initialize CodeMirror in the #editor div
 const cmDiv = document.getElementById('editor');
-let editor = new EditorView({
+const editor = new EditorView({
     state: EditorState.create({
         doc: '',
         extensions: [basicSetup, markdown()],
     }),
     parent: cmDiv,
 });
-
-editor.dispatch = function (tr) {
-    EditorView.prototype.dispatch.call(this, tr);
-    updateCounts();
-}.bind(editor);
